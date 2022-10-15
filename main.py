301c961800101741e277e860bf62be05481fcdc2
qzart45hi <qwert45hi@yandex.ru>
2022-10-15 18:47:40 +0300

    feat(U01): recognizing coords from console

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 
 from os import system, name
 
+UPPER = ord("A")
+LOWER = ord("a")
+
 
 def clear():
     system("cls" if name == "nt" else "clear")
@@ -44,7 +47,45 @@ class Board:
         clear()
         return "".join(self._output())
 
+    def coord_convert(self, coord: str) -> int:
+        if not isinstance(coord, str):
+            raise ValueError("Coord is not a string")
+        if len(coord) != 2:
+            raise ValueError("Coord's length is not 2")
+
+        # numerics are ok
+        if coord.isdigit():
+            result: int = int(coord) - 1
+            if result < 0 or result > self.factor * self.lines:
+                raise ValueError("Coord out of range")
+            return result
+
+        # converting strings
+        place: int = ord(coord[0])
+        if UPPER <= place < UPPER + self.lines:
+            place -= UPPER
+        elif LOWER <= place < LOWER + self.lines:
+            place -= LOWER
+        else:
+            raise ValueError("Coord's first symbol out of range")
+
+        if not coord[1].isdigit():
+            raise ValueError("Coord's second symbol is not a number")
+        # line: int = int(coord[1]) - 1
+        line: int = self.lines - int(coord[1])  # counting from the top (black's side)
+        if line % 2 == place % 2:
+            raise ValueError("Coord can't be a white space")
+
+        return line * self.factor + place // 2
+
 
 if __name__ == "__main__":
     b = Board()
     print(b.output())
+    while True:
+        while True:
+            try:
+                print("Detected coord:", b.coord_convert(input()))
+                break
+            except ValueError as e:
+                print(e)
